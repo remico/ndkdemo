0f4b8b4b05f59209fcf480f46ff2564a9b8c363d
akit nekot <nekotakitapps@gmail.com>
2024-04-01 01:39:21 +0300

    add compose animation for buttons visibility

--- app/src/main/kotlin/it/nekotak/ndkdemo/MainActivity.kt
+++ app/src/main/kotlin/it/nekotak/ndkdemo/MainActivity.kt
@@ -3,6 +3,10 @@ package it.nekotak.ndkdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,6 +19,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -37,6 +45,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(jniWrapper: JNIWrapper, text: String, modifier: Modifier = Modifier) {
+    var isAuthenticated by remember { mutableStateOf(false) }
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
@@ -46,23 +55,43 @@ fun Greeting(jniWrapper: JNIWrapper, text: String, modifier: Modifier = Modifier
             modifier = modifier
         )
         Spacer(modifier = Modifier.height(20.dp))
-        Button(onClick = {
-            jniWrapper.authenticate("192.168.1.106")
-        }) {
-            Text(text = "Authenticate")
-        }
-        Spacer(modifier = Modifier.height(20.dp))
-        Row {
+        AnimatedVisibility(
+            visible = !isAuthenticated,
+            enter = expandVertically(animationSpec = tween(durationMillis = 1000)),
+            exit = shrinkVertically(animationSpec = tween(durationMillis = 1000))
+        ) {
             Button(onClick = {
-                jniWrapper.startLivePreview()
+                jniWrapper.authenticate("192.168.1.106")
+                if (!isAuthenticated) {
+                    isAuthenticated = true
+                }
             }) {
-                Text(text = "Start live preview")
+                Text(text = "Authenticate")
             }
-            Spacer(modifier = Modifier.width(20.dp))
-            Button(onClick = {
-                jniWrapper.stopLivePreview()
-            }) {
-                Text(text = "Stop live preview")
+        }
+        AnimatedVisibility(
+            visible = isAuthenticated,
+            enter = expandVertically(animationSpec = tween(durationMillis = 1000)),
+            exit = shrinkVertically(animationSpec = tween(durationMillis = 1000))
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Spacer(modifier = Modifier.height(20.dp))
+                Row {
+                    Button(onClick = {
+                        jniWrapper.startLivePreview()
+                    }) {
+                        Text(text = "Start live preview")
+                    }
+                    Spacer(modifier = Modifier.width(20.dp))
+                    Button(onClick = {
+                        jniWrapper.stopLivePreview()
+                    }) {
+                        Text(text = "Stop live preview")
+                    }
+                }
             }
         }
     }
